0a510b3131bf73089aaa37f734b39d542087b872
Py-Za <holytheship@gmail.com>
2020-05-21 19:33:03 +0200

    returnuje nie printuje

--- pyzucha01.py
+++ pyzucha01.py
@@ -1,8 +1,8 @@
 def do_shit(str_1, list_1):
     for x in list_1:
         str_1 = str_1.replace(x, "")
-    print(str_1)
+    return str_1
 
 
-do_shit("qwerty", ["we", "qr"])
-print("kurwo")
+print(do_shit("qwerty", ["we", "qr"]), "kurwo")
+
