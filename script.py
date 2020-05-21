8771d017beb7d875be84e716f4f345814db48113
Szymek <radeksekula@gmail.com>
2020-05-21 19:39:06 +0200

    return nie print

--- script.py
+++ script.py
@@ -1,7 +1,8 @@
 def string_cleanup(arg1, arg2):
     for i in arg2:
         arg1 = arg1.replace(i,"")
-    print(arg1)
+    return arg1
 
 
 string_cleanup("abcdbabc_ac_ab_ca", ["ab", "ac"])
+print(string_cleanup("abcdbabc_ac_ab_ca", ["ab", "ac"]))
