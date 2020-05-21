be583ed0f18fb6d1a922783bb4764ced7acbff54
Py-Za <holytheship@gmail.com>
2020-05-21 20:51:27 +0200

    modulo zapisane ladniej

--- pyzucha01.py
+++ pyzucha01.py
@@ -38,14 +38,15 @@ print(safe.open("ArkadiuszNornica", "SzukamNornicyWMojejOkolicy"))  # printuje (
 
 # zadanie nr 3
 def fizzbuzz(x):
-    if x%15 == 0:
+    if x % 15 == 0:
         return "FizzBuzz"
-    elif x%3 == 0:
+    elif x % 3 == 0:
         return "Fizz"
-    elif x%5 == 0:
+    elif x % 5 == 0:
         return "Buzz"
     else:
         return x
+
 
 for i in range(1, 21):
     print(fizzbuzz(i))
